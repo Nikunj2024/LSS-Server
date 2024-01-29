62cccb11ea5723306c655f6f52321d4ed7a683da
Mustafa-livegage <mpanjiwala@livegage.ai>
2024-01-29 18:52:54 +0530

    idk what has been changed

--- Model/Escrow.cs
+++ Model/Escrow.cs
@@ -1,4 +1,3 @@
-
 using System.ComponentModel.DataAnnotations;
 
 namespace LSS.Model
@@ -9,13 +8,11 @@ namespace LSS.Model
         {
         }
 
-        public Escrow(int id, string item_name, string freq, string escrow_type, int amt, string beneficiary_name, string bank_name, long account_number, long route_number, string mop)
+        public Escrow(int id, string item_name, string escrow_type, string beneficiary_name, string bank_name, long account_number, long route_number, string mop)
         {
             this.id = id;
             this.item_name = item_name;
-            this.freq = freq;
             this.escrow_type = escrow_type;
-            this.amt = amt;
             this.beneficiary_name = beneficiary_name;
             this.bank_name = bank_name;
             this.account_number = account_number;
@@ -26,9 +23,7 @@ namespace LSS.Model
         [Key]
         public int id { get; set; }
         public string item_name { get; set; }
-        public string freq { get; set; }
         public string escrow_type { get; set; }
-        public int amt { get; set; }
         public string beneficiary_name { get; set; }
         public string bank_name { get; set; }
         public long account_number { get; set; }
